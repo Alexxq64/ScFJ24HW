9091a0cff89b0e3541a63bf2a2320ad3c6c1f4e5
Alex Kuznetsov <alexxq@list.ru>
2023-07-08 18:42:07 +0300

    Initial commit

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Student {
+    private String fullName;
+    private String universityId;
+    private int currentCourseNumber;
+    private float avgExamScore;
+}
